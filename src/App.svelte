ad205801ee9021d7118ed821f80621d7235c3274
Dave Kozma <dkozma@adobe.com>
2020-08-24 16:48:31 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Router } from '@sveltech/routify';
+  import { routes } from '../.routify/routes';
+</script>
+
+<style global>
+  @import 'tailwindcss/base';
+  @import 'tailwindcss/components';
+  @import 'tailwindcss/utilities';
+  @import '../static/global.css';
+</style>
+
+<Router {routes} />
---
+++ src/pages/index.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { metatags } from "@sveltech/routify";
+  metatags.title = "CAI Verify";
+  metatags.description = "CAI Verify Site";
+</script>
+
+<style lang="postcss">
+  div {
+    @apply text-blue-900 font-bold;
+  }
+</style>
+
+<div>This will be the CAI Verify site!</div>
