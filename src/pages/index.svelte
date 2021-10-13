4c47c3b53a61266bcad1a8252eaee4a5d6e594c5
Natalie Leuenberger <leuenber@adobe.com>
2021-10-13 13:43:58 -0700

    Putting up for PR. Outstanding items: add scaling for 4k, typekit font, scaling for header

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -9,13 +9,13 @@
   import Arrow from '../../assets/svg/monochrome/arrow-back.svg';
 
   // Section 0
-  import hero from '../../assets/png/hero.png';
+  import hero from '../../assets/png/hero@2x.png';
   // Section 1
-  import composition from '../../assets/png/composition@2x.png';
+  import composition from '../../assets/png/section1@2x.png';
   // Section 2 image
-  import dancer from '../../assets/png/dancer@2x.png';
+  import dancer from '../../assets/png/section2@2x.png';
   // Section 3 image
-  import snowyGiza from '../../assets/png/snowy-giza@2x.png';
+  import snowyGiza from '../../assets/png/section3@2x.png';
 
   function upload(evt: Event) {
     window.newrelic?.addPageAction('uploadClick');
@@ -28,28 +28,29 @@
 <div class="theme-light overflow-show">
   <Header />
   <section id="section0" class="overflow-hidden">
-    <div
-      class="w-screen object-fill bg-cover bg-center"
-      style="max-height: 600px; min-height: 500px; background-image: url({heroBkg});" />
-    <div class="hero grid grid-cols-6 gap-4">
-      <div class="w-full col-span-6 lg:col-span-3 order-1">
-        <div
-          class="text-white lg:text-8xl text-7xl font-bold font-serif text-center">
+    <div class="hero grid grid-cols-10">
+      <div
+        class="row-span-full col-start-1 col-span-5 self-center order-3 ml-16">
+        <div class="text-white lg:text-8xl text-7xl font-bold font-serif">
           {$_('page.hero.tagline')}
         </div>
-        <div class="text-white lg:text-5xl text-3xl font-normal text-center">
+        <div class="text-white lg:text-5xl text-3xl font-normal">
           {$_('page.hero.description')}
         </div>
+        <sp-button
+          variant="primary"
+          class="font-bold text-400 mt-8 font-base sm:hidden"
+          on:click={upload}>
+          {$_('comp.header.uploadImage')}
+        </sp-button>
       </div>
-      <div class="w-full lg:col-span-4 order-2 sm:hidden">
+      <div
+        class="row-span-full col-span-6 col-end-11 self-center order-2 mr-16">
         <img src={hero} alt="Person taking picture with smartphone" />
       </div>
-      <sp-button
-        variant="primary"
-        class="relative font-bold text-400 font-base mt-14 sm:hidden"
-        on:click={upload}>
-        {$_('comp.header.uploadImage')}
-      </sp-button>
+      <div
+        class="row-span-full object-fill bg-cover bg-center order-1 col-start-1 col-span-full"
+        style="min-height: 500px; background-image: url({heroBkg});" />
     </div>
   </section>
   <section id="section1">
@@ -58,13 +59,17 @@
         <img src={composition} alt="Composition of woman and city skyline" />
       </div>
       <div
-        class="w-full col-span-3 lg:col-span-1 order-2 justify-center items-center flex px-4 sm:pb-5">
+        class="w-full col-span-3 lg:col-span-1 order-2 lg:justify-center items-center flex px-4 sm:pb-5">
         <div>
-          <div class="heading">{$_('page.composition.header')}</div>
-          <div class="body">{$_('page.composition.body')}</div>
-          <div class="flex">
-            <div class="cta">{$_('page.cta.viewMore')}</div>
-            <Arrow width="14px" height="12px" />
+          <div class="heading lg:text-5xl">
+            {$_('page.composition.header')}
+          </div>
+          <div class="body lg:text-xl">{$_('page.composition.body')}</div>
+          <div class="inline-block align-middle">
+            <div class="cta lg:text-xl sm:text-smd">
+              {$_('page.cta.viewMore')}
+            </div>
+            <Arrow class="inline-block" width="14px" height="12px" />
           </div>
         </div>
       </div>
@@ -76,13 +81,17 @@
         <img src={dancer} alt="Dancer in studio stretching" />
       </div>
       <div
-        class="w-full col-span-3 lg:col-span-1 lg:order-1 justify-center items-center flex lg:pl-8 pl-4 pr-4 sm:pb-5">
+        class="w-full col-span-3 lg:col-span-1 lg:order-1 lg:justify-center items-center flex lg:pl-8 pl-4 pr-4 sm:pb-5">
         <div>
-          <div class="heading">{$_('page.dancer.header')}</div>
-          <div class="body">{$_('page.dancer.body')}</div>
-          <div class="flex">
-            <div class="cta">{$_('page.cta.viewMore')}</div>
-            <Arrow width="14px" height="12px" />
+          <div class="heading lg:text-5xl">
+            {$_('page.dancer.header')}
+          </div>
+          <div class="body lg:text-xl">{$_('page.dancer.body')}</div>
+          <div class="inline-block align-middle">
+            <div class="cta lg:text-xl sm:text-smd">
+              {$_('page.cta.viewMore')}
+            </div>
+            <Arrow class="inline-block" width="14px" height="12px" />
           </div>
         </div>
       </div>
@@ -94,13 +103,17 @@
         <img src={snowyGiza} alt="Snowy Giza" />
       </div>
       <div
-        class="w-full col-span-3 lg:col-span-1 order-2 justify-center items-center flex px-4 sm:pb-5">
+        class="w-full col-span-3 lg:col-span-1 order-2 lg:justify-center items-center flex px-4 sm:pb-5">
         <div>
-          <div class="heading">{$_('page.snowyGiza.header')}</div>
-          <div class="body">{$_('page.snowyGiza.body')}</div>
-          <div class="flex">
-            <div class="cta">{$_('page.cta.viewMore')}</div>
-            <Arrow width="14px" height="12px" />
+          <div class="heading lg:text-5xl">
+            {$_('page.snowyGiza.header')}
+          </div>
+          <div class="body lg:text-xl">{$_('page.snowyGiza.body')}</div>
+          <div class="inline-block align-middle">
+            <div class="cta lg:text-xl sm:text-smd">
+              {$_('page.cta.viewMore')}
+            </div>
+            <Arrow class="inline-block" width="14px" height="12px" />
           </div>
         </div>
       </div>
@@ -115,7 +128,7 @@
     @apply font-base;
   }
   .hero {
-    @apply absolute inset-0 flex justify-center items-center flex-col;
+    /* @apply absolute inset-0 flex justify-center items-center flex-col; */
     width: 100vw;
   }
   section {
@@ -125,16 +138,17 @@
   }
 
   .heading {
-    @apply text-black font-serif lg:text-5xl text-3xl text-left lg:pb-14 sm:pb-5;
+    @apply text-black font-serif text-3xl text-left pb-4;
   }
 
   .body {
-    @apply text-black lg:text-xl text-smd text-left pb-5;
+    @apply text-black text-smd text-left pb-4;
   }
 
   .cta {
-    @apply text-black font-bold lg:text-xl sm:text-smd text-left pb-4;
+    @apply inline-block text-black font-bold text-left pb-4;
   }
+
   sp-button {
     @apply inline-block bg-white px-8 py-4 rounded-full transition duration-200 outline-none text-gray-900;
   }
