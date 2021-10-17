b078a16794f72440eee46ebebe06e6ac109ea76c
Natalie Leuenberger <leuenber@adobe.com>
2021-10-17 16:11:23 -0700

    Desktop Landing Page changes

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -5,17 +5,17 @@
   import Footer from '../components/Footer.svelte';
   import '@spectrum-web-components/button/sp-button.js';
 
-  import heroBkg from '../../assets/svg/home/adjustedHero.noparse.svg';
   import Arrow from '../../assets/svg/monochrome/arrow-back.svg';
+  import DownArrow from '../../assets/svg/monochrome/down-arrow';
 
   // Section 0
-  import hero from '../../assets/png/hero@2x.png';
+  import hero from '../../assets/png/hero-img.png';
   // Section 1
-  import section1 from '../../assets/png/AdobeStock_55134478-edit.png';
+  import section1 from '../../assets/png/section1.jpg';
   // Section 2 image
-  import section2 from '../../assets/png/original-drawing2.png';
+  import section2 from '../../assets/png/section2.png';
   // Section 3 image
-  import section3 from '../../assets/png/ThePencilTape.png';
+  import section3 from '../../assets/png/section3.png';
 
   function upload(evt: Event) {
     window.newrelic?.addPageAction('uploadClick');
@@ -27,33 +27,30 @@
 <div class="theme-light overflow-show">
   <Header />
   <section id="section0" class="overflow-hidden">
-    <div class="hero grid grid-cols-10">
+    <div class="hero grid grid-cols-10 my-10">
       <div
-        class="row-span-full col-start-1 col-span-5 self-center order-3 ml-16">
-        <div class="text-white lg:text-8xl text-7xl font-bold font-home">
+        class="row-span-full col-start-1 col-span-5 self-center order-3 ml-16 mr-8">
+        <div class="lg:text-8xl text-7xl font-bold font-home">
           {$_('page.hero.tagline')}
         </div>
-        <div class="text-white lg:text-5xl text-3xl font-normal">
+        <div class="lg:text-5xl text-3xl font-normal">
           {$_('page.hero.description')}
         </div>
-        <sp-button
-          variant="primary"
-          class="font-bold text-400 mt-8 font-base sm:hidden"
-          on:click={upload}>
-          {$_('comp.header.uploadImage')}
-        </sp-button>
       </div>
       <div
-        class="overlap row-span-full col-span-6 col-end-11 self-center order-2 mr-16 object-contain">
+        class="overlap row-span-full col-span-5 col-end-11 self-center order-2 mr-16 object-contain">
         <img src={hero} alt="Person taking picture with smartphone" />
       </div>
-      <div
-        class="row-span-full object-fill bg-cover bg-center order-1 col-start-1 col-span-full"
-        style="min-height: 500px; background-image: url({heroBkg});" />
+      <a href="#section1">
+        <DownArrow
+          class="animate-bounce col-start-1 ml-16 mt-2 -mb-2"
+          width="48px"
+          height="48px" />
+      </a>
     </div>
   </section>
   <section id="section1">
-    <div class="grid grid-cols-3 gap-4">
+    <div class="grid grid-cols-3 gap-4 mx-16 my-10">
       <div class="w-full col-span-3 lg:col-span-2 order-1">
         <img src={section1} alt="Icy lake at dusk" />
       </div>
@@ -63,7 +60,8 @@
           <div class="heading lg:text-5xl">
             {$_('page.sectionOne.header')}
           </div>
-          <div class="body lg:text-xl">{$_('page.sectionOne.body')}</div>
+          <div class="body lg:text-xl">{$_('page.sectionOne.bodyA')}</div>
+          <div class="body lg:text-xl">{$_('page.sectionOne.bodyB')}</div>
           <div class="inline-block align-middle">
             <div class="cta lg:text-xl sm:text-smd">
               {$_('page.cta.viewMore')}
@@ -75,7 +73,7 @@
     </div>
   </section>
   <section id="section2">
-    <div class="grid grid-cols-3 gap-4">
+    <div class="grid grid-cols-3 gap-4 mx-16 my-10">
       <div class="w-full col-span-3 lg:col-span-2 lg:order-2">
         <img src={section2} alt="Yellow vector art" />
       </div>
@@ -85,7 +83,8 @@
           <div class="heading lg:text-5xl">
             {$_('page.sectionTwo.header')}
           </div>
-          <div class="body lg:text-xl">{$_('page.sectionTwo.body')}</div>
+          <div class="body lg:text-xl">{$_('page.sectionTwo.bodyA')}</div>
+          <div class="body lg:text-xl">{$_('page.sectionTwo.bodyB')}</div>
           <div class="inline-block align-middle">
             <div class="cta lg:text-xl sm:text-smd">
               {$_('page.cta.viewMore')}
@@ -97,7 +96,7 @@
     </div>
   </section>
   <section id="section3">
-    <div class="grid grid-cols-3 gap-4">
+    <div class="grid grid-cols-3 gap-4 mx-16 my-10">
       <div class="w-full col-span-3 lg:col-span-2 order-1">
         <img src={section3} alt="Kaleidescope vector art" />
       </div>
@@ -107,7 +106,8 @@
           <div class="heading lg:text-5xl">
             {$_('page.sectionThree.header')}
           </div>
-          <div class="body lg:text-xl">{$_('page.sectionThree.body')}</div>
+          <div class="body lg:text-xl">{$_('page.sectionThree.bodyA')}</div>
+          <div class="body lg:text-xl">{$_('page.sectionThree.bodyB')}</div>
           <div class="inline-block align-middle">
             <div class="cta lg:text-xl sm:text-smd">
               {$_('page.cta.viewMore')}
@@ -150,9 +150,5 @@
 
   .cta {
     @apply inline-block text-black font-bold text-left pb-4 font-home;
-  }
-
-  sp-button {
-    @apply inline-block bg-white px-8 py-4 rounded-full transition duration-200 outline-none text-gray-900;
   }
 </style>
