198455e4fdd0a4c50f8ccc49ff36209a23c6b63e
Natalie Leuenberger <leuenber@adobe.com>
2021-11-02 13:38:44 -0700

    Fix sh7642 and sh7717

--- src/components/inspect/TopNavigation.svelte
+++ src/components/inspect/TopNavigation.svelte
@@ -12,6 +12,7 @@
     CompareMode,
     isMobileViewerShown,
     navigateToPath,
+    hierarchy,
   } from '../../stores';
   import { Source } from '../../lib/sdk';
   import BreadcrumbDropdown from '../../../assets/svg/monochrome/breadcrumb-dropdown.svg';
@@ -44,6 +45,11 @@
 
   $: showMenu = $isMobileViewerShown;
   $: nodeAncestors = $ancestors;
+  $: thumbnailAsset =
+    $hierarchy?.data?.id === '__source__'
+      ? $hierarchy?.data?.asset
+      : $primaryAsset?.asset;
+  $: thumbnailTitle = $primaryAsset?.title || $primaryAsset?.filename;
 </script>
 
 <div id="breadcrumb-bar" class="container" class:menu-view={showMenu}>
@@ -78,7 +84,7 @@
         </div>
       {:else if showMenu}
         <div class="flex self-center">
-          {#if nodeAncestors.length > 1}
+          {#if nodeAncestors?.length > 1}
             <sp-action-menu class="-ml-3" value={$primaryId}>
               <div slot="icon" class="py-2">
                 <BreadcrumbDropdown
@@ -105,10 +111,8 @@
             </div>
           {/if}
           <div class="breadcrumb-item items-center current">
-            <Thumbnail asset={$primaryAsset?.asset} />
-            <span class="font-regular text-smd ml-2"
-              >{$primaryAsset?.title}
-            </span>
+            <Thumbnail asset={thumbnailAsset} />
+            <span class="font-regular text-smd ml-2">{thumbnailTitle} </span>
           </div>
         </div>
       {:else}
