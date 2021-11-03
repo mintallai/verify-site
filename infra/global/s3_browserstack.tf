026b8f090aedc882835f41b53422a77671a2c4bc
Jack Farzan <jfarzan@adobe.com>
2021-11-03 15:47:10 -0400

    added cors rule

--- infra/global/s3_browserstack.tf
+++ infra/global/s3_browserstack.tf
@@ -1,8 +1,17 @@
 module "s3_browserstack" {
-  source = "git@git.corp.adobe.com:cai/shared-infra.git"
+  source = "git@git.corp.adobe.com:cai/shared-infra.git//modules/s3-bucket?ref=cors-s3"
 
   shortname = "browserstack"
   service   = "verify-site"
+  use_cors  = true
+
+  cors_rule = [{
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://s3-website-test.hashicorp.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = ["3000"]
+  }]
 }
 
 output "s3_browserstack_bucket_name" {
