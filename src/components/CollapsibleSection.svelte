99d56c03516023b6f8d97403391248f793fd9d3e
Massy Mahamli <mmahamli@Massys-MacBook-Pro.local>
2022-04-28 05:57:11 -0400

    1145 , 1146 and 480

---
+++ src/components/CollapsibleSection.svelte
@@ -0,0 +1,37 @@
+<script>
+  // based on suggestions from:
+  // Inclusive Components by Heydon Pickering https://inclusive-components.design/collapsible-sections/
+  export let headerText;
+  export let helper;
+  import DownArrow from '../../assets/svg/monochrome/down-arrow.svg';
+  let expanded = true;
+
+  function expand() {
+    expanded = !expanded;
+  }
+</script>
+
+<div>
+  <h3 class="flex justify-between">
+    <button
+      class="flex font-bold text-75 uppercase text-gray-700"
+      on:click={() => expand()}>
+      <DownArrow
+        class="h-4 w-4 text-gray-500 mr-1 transform {expanded
+          ? 'rotate-0'
+          : '-rotate-90'}" />
+      {headerText}
+    </button>
+    <cai-tooltip placement="left" class="theme-spectrum">
+      <div slot="content" class="text-gray-900" style="width: 200px;">
+        {helper}
+      </div>
+    </cai-tooltip>
+  </h3>
+
+  <div hidden={!expanded} class="relative top-1.5">
+    <div>
+      <slot />
+    </div>
+  </div>
+</div>
--- src/components/ManifestDetails.svelte
+++ src/components/ManifestDetails.svelte
@@ -30,11 +30,10 @@
   import '@contentauth/web-components/dist/components/Tooltip';
   import '@contentauth/web-components/dist/themes/spectrum';
   import type { HierarchyTreeNode } from '../stores';
-
+  import AboutSection from './inspect/AboutSection.svelte';
   export let node: HierarchyTreeNode;
   export let isComparing: boolean = false;
   export let isMobileViewer: boolean = false;
-
   $: manifest = getManifest(node);
   $: isOriginal = getIsOriginal(node);
   $: generator = manifest.claimGenerator.product;
@@ -52,205 +51,172 @@
 </script>
 
 <div>
-  <dl class="attributes">
-    <dt>
-      <div>{$_('comp.about.signedBy')}</div>
-      <cai-tooltip placement="left" class="theme-spectrum">
-        <div slot="content" class="text-gray-900" style="width: 200px;">
-          {$_('comp.about.signedBy.helpText')}
+  <dl>
+    <AboutSection
+      title={$_('comp.about.signedBy')}
+      helper={$_('comp.about.signedBy.helpText')}
+      collapsible="true">
+      <dd class="flex space-x-2 !mb-0 " data-test-id="about.signed-by">
+        <div class="relative top-0.5">
+          <ProviderIcon provider={issuer} />
         </div>
-      </cai-tooltip>
-    </dt>
-    <dd class="flex space-x-2" data-test-id="about.signed-by">
-      <div class="relative top-0.5">
-        <ProviderIcon provider={issuer} />
-      </div>
-      <div>
-        {issuer ?? ''}
-      </div>
-    </dd>
-    <dt>
-      <div>{$_('comp.about.signedOn')}</div>
-      <cai-tooltip placement="left" class="theme-spectrum">
-        <div slot="content" class="text-gray-900" style="width: 200px;">
-          {$_('comp.about.signedOn.helpText')}
+        <div>
+          {issuer ?? ''}
         </div>
-      </cai-tooltip>
-    </dt>
-    <dd data-test-id="about.signed-on">
-      {#if sigDate && sigDate.toString() !== 'Invalid Date'}
-        {$date(sigDate, { format: 'short' })}{', '}
-        {$time(sigDate, { format: 'short' })}
-      {:else}
-        {$_('comp.about.signedOn.notAvailable')}
-      {/if}
-    </dd>
+      </dd>
+
+      <div data-test-id="about.signed-on">
+        {#if sigDate && sigDate.toString() !== 'Invalid Date'}
+          <div class="text-gray-700 -top-1.5 ml-6 ">
+            {$date(sigDate, { format: 'long' })}{' at '}
+            {$time(sigDate, { format: 'short' })}
+          </div>
+        {:else}
+          {$_('comp.about.signedOn.notAvailable')}
+        {/if}
+      </div>
+    </AboutSection>
   </dl>
 </div>
 <div>
-  <dl class="attributes" data-test-id="about.produced-with">
-    <dt>
-      <div>{$_('comp.about.producedWith')}</div>
-      <cai-tooltip placement="left" class="theme-spectrum">
-        <div slot="content" class="text-gray-900" style="width: 200px;">
-          {$_('comp.about.producedWith.helpText')}
+  <dl data-test-id="about.produced-with">
+    <AboutSection
+      title={$_('comp.about.producedWith')}
+      helper={$_('comp.about.producedWith.helpText')}
+      collapsible="true">
+      <dd class="flex">
+        <div class="relative top-0.5">
+          <ProviderIcon provider={generator} class="mr-2" />
         </div>
-      </cai-tooltip>
-    </dt>
-    <dd class="flex">
-      <div class="relative top-0.5">
-        <ProviderIcon provider={generator} class="mr-2" />
-      </div>
-      <div class="break-word">
-        <div>{generator}</div>
-        {#if isBeta}
-          <div class="text-gray-700">Content Credentials (Beta)</div>
-        {/if}
-      </div>
-    </dd>
+        <div class="break-word">
+          <div>{generator}</div>
+          {#if isBeta}
+            <div class="text-gray-700">Content Credentials (Beta)</div>
+          {/if}
+        </div>
+      </dd>
+    </AboutSection>
   </dl>
 </div>
-{#await manifest.editsAndActivity() then categories}
-  {#if categories}
-    <div>
-      <dl class="attributes">
-        <dt>
-          <div class="whitespace-nowrap">
-            {$_('comp.about.editsActivity.header')}
-          </div>
-          <cai-tooltip placement="left" class="theme-spectrum">
-            <div slot="content" class="text-gray-900" style="width: 200px;">
-              {$_('comp.about.editsActivity.helpText')}
-            </div>
-          </cai-tooltip>
-        </dt>
-        {#if ratings.hasUnknownActions || ratings.wasPossiblyModified}
-          <dd data-test-id="about.unknownActionsAlert" class="flex mt-1">
-            <div class="relative top-0.5">
-              <AlertOutlineIcon width="16px" height="16px" class="mr-2" />
-            </div>
-            <div class="italic text-gray-900">
-              <span>{$_('comp.contentCredentialsError.unknownActions')}</span>
-              <a href={getFaqUrl()} target="_blank" class="link"
-                >{$_('comp.contentCredentialsError.learnMore')}</a>
-            </div>
+<AboutSection
+  title={$_('comp.about.editsActivity.header')}
+  helper={$_('comp.about.editsActivity.helpText')}
+  collapsible="true">
+  {#await manifest.editsAndActivity() then categories}
+    {#if categories}
+      <div>
+        <dl>
+          {#if ratings.hasUnknownActions || ratings.wasPossiblyModified}
+            <dd data-test-id="about.unknownActionsAlert" class="flex mt-1">
+              <div class="relative top-0.5">
+                <AlertOutlineIcon width="16px" height="16px" class="mr-2" />
+              </div>
+              <div class="italic text-gray-900">
+                <span>{$_('comp.contentCredentialsError.unknownActions')}</span>
+                <a href={getFaqUrl()} target="_blank" class="link"
+                  >{$_('comp.contentCredentialsError.learnMore')}</a>
+              </div>
+            </dd>
+          {/if}
+          <dd class="mt-2" data-test-id="about.edits-and-activity">
+            <cai-panel-edits-activity
+              {categories}
+              stringmap={editsActivityStrings}
+              hidedescriptions={isMobileViewer && isComparing ? true : null}
+              class="theme-spectrum" />
           </dd>
-        {/if}
-        <dd class="mt-2" data-test-id="about.edits-and-activity">
-          <cai-panel-edits-activity
-            {categories}
-            stringmap={editsActivityStrings}
-            hidedescriptions={isMobileViewer && isComparing ? true : null}
-            class="theme-spectrum" />
-        </dd>
-      </dl>
-    </div>
-  {/if}
-{/await}
+        </dl>
+      </div>
+    {/if}
+  {/await}
+</AboutSection>
 <div>
   {#if isOriginal}
     <OriginalCreation type="original" {node} />
   {:else}
-    <dl class="attributes" data-test-id="about.assets">
-      <dt>
-        <div class="whitespace-nowrap">
-          {$_('comp.about.assets.header')}
-        </div>
-        <cai-tooltip placement="left" class="theme-spectrum">
-          <div slot="content" class="text-gray-900" style="width: 200px;">
-            {$_('comp.about.assets.helpText')}
-          </div>
-        </cai-tooltip>
-      </dt>
-      <dd class="pt-2 pb-1">
-        {#if node.children.length}
-          <div class="assets-used">
-            {#each node.children as childNode}
-              <div
-                class="w-12 h-12 cursor-pointer"
-                on:click={() => navigateToChild(childNode.data.loc)}>
-                <Thumbnail node={childNode} {...getBadgeProps(childNode)} />
-              </div>
-            {/each}
-          </div>
-        {:else}
-          {$_('comp.about.none')}
-        {/if}
-      </dd>
-    </dl>
+    <AboutSection
+      title={$_('comp.about.assets.header')}
+      helper={$_('comp.about.assets.helpText')}
+      collapsible="true">
+      <dl data-test-id="about.assets">
+        <dd class="pt-2 pb-1">
+          {#if node.children.length}
+            <div class="assets-used">
+              {#each node.children as childNode}
+                <div
+                  class="w-12 h-12 cursor-pointer"
+                  on:click={() => navigateToChild(childNode.data.loc)}>
+                  <Thumbnail node={childNode} {...getBadgeProps(childNode)} />
+                </div>
+              {/each}
+            </div>
+          {:else}
+            {$_('comp.about.none')}
+          {/if}
+        </dd>
+      </dl>
+    </AboutSection>
   {/if}
 </div>
 {#if producer}
   <div>
-    <dl class="attributes" data-test-id="about.produced-by">
-      <dt class="flex space-x-2">
-        <div class="whitespace-nowrap">{$_('comp.about.producedBy')}</div>
-        <cai-tooltip placement="left" class="theme-spectrum">
-          <div slot="content" class="text-gray-900" style="width: 150px;">
-            {$_('comp.about.producedBy.helpText')}
-          </div>
-        </cai-tooltip>
-      </dt>
-      <dd>{producer.name}</dd>
-    </dl>
+    <AboutSection
+      title={$_('comp.about.producedBy')}
+      helper={$_('comp.about.producedBy.helpText')}
+      collapsible="true">
+      <dl data-test-id="about.produced-by">
+        <dd>{producer.name}</dd>
+      </dl>
+    </AboutSection>
   </div>
 {/if}
 {#if website}
   <div>
-    <dl class="attributes">
-      <dt class="flex space-x-2">
-        <div class="whitespace-nowrap">{$_('comp.about.website')}</div>
-        <cai-tooltip placement="left" class="theme-spectrum">
-          <div slot="content" class="text-gray-900" style="width: 150px;">
-            {$_('comp.about.website.helpText')}
-          </div>
-        </cai-tooltip>
-      </dt>
-      <dd class="truncate">
-        <a href={website} target="_blank" class="link">{website}</a>
-      </dd>
-    </dl>
+    <AboutSection
+      title={$_('comp.about.website')}
+      helper={$_('comp.about.website.helpText')}
+      collapsible="true">
+      <dl>
+        <dd class="truncate">
+          <a href={website} target="_blank" class="link">{website}</a>
+        </dd>
+      </dl>
+    </AboutSection>
   </div>
 {/if}
 {#if socialAccounts?.length || web3Addresses?.length}
   <div class="space-y-4">
     {#if socialAccounts?.length}
-      <dl class="attributes">
-        <dt class="flex space-x-2">
-          <div class="whitespace-nowrap">{$_('comp.about.social')}</div>
-          <cai-tooltip placement="left" class="theme-spectrum">
-            <div slot="content" class="text-gray-900" style="width: 150px;">
-              {$_('comp.about.social.helpText')}
-            </div>
-          </cai-tooltip>
-        </dt>
-        <dd class="social-accounts">
-          {#each socialAccounts as account (account['@id'])}
-            <div class="relative top-0.5">
-              <ProviderIcon provider={account['@id']} class="mr-2" />
-            </div>
-            <a
-              href={account['@id']}
-              target="_blank"
-              class="link"
-              data-test-id="about.social-accounts.link">@{account.name}</a>
-          {/each}
-        </dd>
+      <dl>
+        <AboutSection
+          title={$_('comp.about.social')}
+          helper={$_('comp.about.social.helpText')}
+          collapsible="true">
+          <dd class="social-accounts">
+            {#each socialAccounts as account (account['@id'])}
+              <div class="relative top-0.5">
+                <ProviderIcon provider={account['@id']} class="mr-2" />
+              </div>
+              <a
+                href={account['@id']}
+                target="_blank"
+                class="link"
+                data-test-id="about.social-accounts.link">@{account.name}</a>
+            {/each}
+          </dd>
+        </AboutSection>
       </dl>
     {/if}
     {#if web3Addresses?.length}
-      <dl class="attributes">
-        <dt class="flex space-x-2">
-          <div class="whitespace-nowrap">{$_('comp.about.web3')}</div>
-          <cai-tooltip placement="left" class="theme-spectrum">
-            <div slot="content" class="text-gray-900" style="width: 150px;">
-              {$_('comp.about.web3.helpText')}
-            </div>
-          </cai-tooltip>
-        </dt>
-        {#each web3Addresses as [type, [address]]}
-          <Web3Address {type} {address} />
-        {/each}
+      <dl>
+        <AboutSection
+          title={$_('comp.about.web3')}
+          helper={$_('comp.about.web3.helpText')}
+          collapsible="true">
+          {#each web3Addresses as [type, [address]]}
+            <Web3Address {type} {address} />
+          {/each}
+        </AboutSection>
       </dl>
     {/if}
   </div>
