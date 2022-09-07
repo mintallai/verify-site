7ae4a91ab97508fcbb0e8fa716d9af3df6e8da7d
Eli Mensch <mensch@adobe.com>
2022-09-07 16:34:19 -0400

    Update test config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests/unit/'],
+  transformIgnorePatterns: [`node_modules/(?!c2pa)`],
   globals: {
     'ts-jest': {
       tsconfig: 'tests/tsconfig.json',
