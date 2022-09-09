abc7f94572d1f22db2f6875519af7c89587cd390
Massy Mahamli <mmahamli@adobe.com>
2022-09-09 17:24:23 -0400

    changes

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -91,7 +91,7 @@ export function selectIsOriginal(manifest: Manifest) {
   }
 
   const noIngredients = manifest.ingredients?.length === 0;
-  const actions = manifest.assertions.get('c2pa.actions')[0].data.actions;
+  const actions = manifest.assertions.get('c2pa.actions')[0]?.data?.actions;
   const isDelivered = actions?.some((x) => x.action === 'adobe.delivered');
 
   return noIngredients && !isDelivered;
