160904f8d07f10439c536a2edb9149c045f1c335
Massy Mahamli <mmahamli@adobe.com>
2023-01-11 14:19:38 -0500

    tooltip work

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { TippyProps } from '../lib/tippy';
+  import { tippy } from '../lib/tippy';
+
+  let content;
+  export let placement;
+  const tippyOpts: Partial<TippyProps> = {
+    interactive: true,
+    trigger: 'mouseenter',
+    appendTo: document.body,
+    onShow() {},
+    onHide() {},
+  };
+</script>
+
+<div class="trigger" use:tippy={{ content: content, ...tippyOpts, placement }}>
+  <slot name="trigger">
+    <cai-icon-help />
+  </slot>
+  <slot name="content" {content} />
+</div>
