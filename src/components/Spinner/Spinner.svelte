9bcf93c5b4496e3200af8dcec48e2892faddba62
Dave Kozma <dkozma@adobe.com>
2023-09-05 14:26:45 -0400

    Translate spinner label

--- src/components/Spinner/Spinner.svelte
+++ src/components/Spinner/Spinner.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import '@spectrum-web-components/progress-circle/sp-progress-circle.js';
+  import { _ } from 'svelte-i18n';
 
   export let size: 's' | 'm' | 'l' = 's';
 </script>
 
 <sp-theme color="lightest" scale="medium">
   <sp-progress-circle
-    label="Loading asset data"
+    label={$_('spinner.loading')}
     indeterminate="true"
     {size}
     class="loader" />
