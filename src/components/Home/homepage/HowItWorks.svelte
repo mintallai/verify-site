3ef7919013438243e15c5ab4809e88f0c6711d12
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-10-02 13:01:34 -0400

    replaced step3 in hiw with video

--- src/components/Home/homepage/HowItWorks.svelte
+++ src/components/Home/homepage/HowItWorks.svelte
@@ -1,7 +1,9 @@
 <script>
   import Hiw1 from '$assets/svg/logos/homepage/2.svg';
   import Hiw2 from '$assets/svg/logos/homepage/1.svg';
-  import Hiw3 from '$assets/svg/logos/homepage/hiw3.svg';
+  // import Hiw3 from '$assets/svg/logos/homepage/hiw3.svg';
+
+  import Video from '$assets/video/cr-pin.mp4';
 
   const data = [
     {
@@ -23,31 +25,31 @@
       description:
         'Once the content is published, anyone can view the information by clicking on the pin, which reveals basic information about the image directly in context.',
     },
-    {
-      id: 3,
-      label: 'Go deeper to see the full edit history',
-      labelNumber: 3,
-      labelBg: '#fe818b',
-      Image: Hiw3,
-      description:
-        'Content Credentials captures the full edit history of content in a new feature called Verify. Just click “Inspect.” The feature also allows you to upload your own content to see if edit history is available.',
-    },
+    // {
+    //   id: 3,
+    //   label: 'Go deeper to see the full edit history',
+    //   labelNumber: 3,
+    //   labelBg: '#fe818b',
+    //   Image: Hiw3,
+    //   description:
+    //     'Content Credentials captures the full edit history of content in a new feature called Verify. Just click “Inspect.” The feature also allows you to upload your own content to see if edit history is available.',
+    // },
   ];
 </script>
 
 <div
   class="flex h-full w-screen flex-col items-center bg-[#F6F5F2] p-[1.5rem] pb-40">
   <h2
-    class="md:text-hiw-title-md xl:text-hiw-title-xl xxl:text-hiw-title-xxl pt-[4rem] text-hiw-title">
+    class="xxl:text-hiw-title-xxl pt-[4rem] text-hiw-title md:text-hiw-title-md xl:text-hiw-title-xl">
     How it Works
   </h2>
   <div class="flex w-full flex-col items-center">
     <div class="">
       {#each data as item}
         <div
-          class="pt-[96px] lg:max-w-[1145px] xl:max-w-[116rem] xl:pt-[128px] xl:px-[10rem] xxl:pt-[160px] max-w-[90rem]">
+          class="max-w-[90rem] pt-[96px] lg:max-w-[1145px] xl:max-w-[116rem] xl:px-[10rem] xl:pt-[128px] xxl:pt-[160px]">
           <div
-            class="flex max-w-[86rem] flex-col items-start justify-between sm:pb-[3.5rem] xl:pb-[4rem] lg:flex-row">
+            class="flex max-w-[86rem] flex-col items-start justify-between sm:pb-[3.5rem] lg:flex-row xl:pb-[4rem]">
             <div class="flex flex-row items-start justify-start gap-4 lg:w-1/2">
               <div
                 class="mr-0.5 flex h-[22px] w-[22px] items-center justify-center rounded-full px-4 py-4"
@@ -59,7 +61,7 @@
               </h3>
             </div>
             <p
-              class="xxl:text-hiw-desc-xxl pt-6 text-hiw-desc text-gray-900/60 lg:w-1/2 lg:pt-0 xl:text-hiw-desc-xl">
+              class="pt-6 text-hiw-desc text-gray-900/60 lg:w-1/2 lg:pt-0 xl:text-hiw-desc-xl xxl:text-hiw-desc-xxl">
               {item.description}
             </p>
           </div>
@@ -69,6 +71,34 @@
             class="max-w[71.375rem] rounded-xxl xxl:w-full" />
         </div>
       {/each}
+      <!-- Video Component -->
+      <div
+        class="max-w-[90rem] pt-[96px] lg:max-w-[1145px] xl:max-w-[116rem] xl:px-[10rem] xl:pt-[128px] xxl:pt-[160px]">
+        <div
+          class="flex max-w-[86rem] flex-col items-start justify-between sm:pb-[3.5rem] lg:flex-row xl:pb-[4rem]">
+          <div class="flex flex-row items-start justify-start gap-4 lg:w-1/2">
+            <div
+              class="mr-0.5 flex h-[22px] w-[22px] items-center justify-center rounded-full bg-brand-red px-4 py-4">
+              3
+            </div>
+            <h3 class="pr-10 text-hiw-label xl:text-hiw-label-xl">
+              Go deeper to explore the full edit history
+            </h3>
+          </div>
+          <p
+            class="pt-6 text-hiw-desc text-gray-900/60 lg:w-1/2 lg:pt-0 xl:text-hiw-desc-xl xxl:text-hiw-desc-xxl">
+            Content Credentials can capture a detailed history of changes over
+            time. The Verify feature allows you to explore this information in
+            depth, and upload any content to see if it has Content Credentials.
+          </p>
+        </div>
+        <video
+          autoplay
+          loop
+          muted
+          src={Video}
+          class="h-full max-h-full w-full max-w-full rounded-xl object-cover xl:h-full" />
+      </div>
     </div>
   </div>
 </div>
