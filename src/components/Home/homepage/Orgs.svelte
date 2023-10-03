4bd4f5e70f2226928f47b0e979681669bcb2a5a6
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-10-03 16:57:52 -0400

    Manually added Pari's updates

--- src/components/Home/homepage/Orgs.svelte
+++ src/components/Home/homepage/Orgs.svelte
@@ -24,7 +24,7 @@
   ];
 </script>
 
-<div class="w-screen pt-[4rem] max-w-[90rem]">
+<div class="w-screen py-16 max-w-[90rem] lg:py-[5.5rem] xl:pb-[7.5rem]">
   <div class="px-6">
     <!-- <div class="w-[350px] pb-4 text-[1.3125rem] leading-[1.77188rem]">
       Content Credentials is used by the world’s leading organizations
--- src/components/Home/homepage/Powered.svelte
+++ src/components/Home/homepage/Powered.svelte
@@ -1,5 +1,5 @@
 <div
-  class="flex w-screen max-w-[90rem] items-center justify-center px-[1.5rem] py-20">
+  class="flex w-screen max-w-[90rem] items-center justify-center px-[1.5rem] py-20 xl:pb-[8.5rem]">
   <div class="flex flex-col items-center justify-evenly gap-[40px]">
     <h2
       class="2xl:text-movement-title-2xl text-center text-movement-title md:w-[40rem] md:text-movement-title-md xl:w-[50rem] xl:text-movement-title-xl">
