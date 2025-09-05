5866ca3372f7c26b6462bca7abbadec86d8bd431
Esther Jun Kim <3721990+estherjk@users.noreply.github.com>
2025-09-05 16:47:11 +0900

    Fix issue when deploying to Vercel

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -7,6 +7,11 @@ import type { LayoutLoad } from './$types';
 
 export const ssr = false;
 
+// Resolves an issue with Vercel and SvelteKit where the first request to a page fails with a 404
+// See: https://stackoverflow.com/a/78189721
+export const prerender = true;
+export const trailingSlash = 'always'; // Added this
+
 export const load: LayoutLoad = async () => {
   if (typeof window !== 'undefined') {
     initI18n();
